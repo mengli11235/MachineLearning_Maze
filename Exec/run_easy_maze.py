dbe6b6b6b44983dd255bd5b8954b1d86bdf9795d
YuYing Chen <y.chen.66@student.rug.nl>
2017-12-06 11:07:28 +0100

    temp version

--- Exec/run_easy_maze.py
+++ Exec/run_easy_maze.py
@@ -14,13 +14,8 @@ from LearningAlgos.easy_maze_RL import QLearningTable
 import pandas as pd
 import time
 
-def running(epi, time_in_ms, is_render, QL, env):
-    try:
-        df = pd.DataFrame.from_csv('temp_q_table.csv', sep=',', encoding='utf8')
-        QL.set_prior_qtable(df)
-    except Exception:
-        pass
 
+def learning(epi, time_in_ms, _is_render, QL, env):
     for episode in range(epi):
         # initiate the agent
         agent = env.reset()
@@ -47,7 +42,7 @@ def running(epi, time_in_ms, is_render, QL, env):
 
     # end of game
     print('game over')
-    if is_render:
+    if _is_render:
         time.sleep(1)
         env.destroy()
 
@@ -55,18 +50,54 @@ def running(epi, time_in_ms, is_render, QL, env):
     print(QL.q_table)
 
 
+def running(epi, time_in_ms, _is_render, QL, env):
+    try:
+        df = pd.DataFrame.from_csv('temp_q_table.csv', sep=',', encoding='utf8')
+        QL.set_prior_qtable(df)
+        print("set prior q")
+    except Exception:
+        pass
+
+    for episode in range(epi):
+        # initiate the agent
+        agent = env.reset()
+
+        while True:
+            # fresh env
+            env.render(time_in_ms)
+
+            # RL choose action based on observation
+            action = QL.choose_action(str(agent))
+
+            # RL take action and get next observation and reward
+            new_state, reward, is_done = env.taking_action(action)
+
+            # swap observation
+            agent = new_state
+
+            # break while loop when end of this episode
+            if is_done:
+                break
+
+    # end of game
+    print('game over')
+    if _is_render:
+        time.sleep(1)
+        env.destroy()
+
+
 if __name__ == "__main__":
     # set if render the GUI
     is_render = False
     # set number of runs
-    episodes = 30000
+    episodes = 50000
     # animation interval
-    interval = 0.02
+    interval = 0.005
     # set the size of maze: column x row
-    size_maze = [4, 8]
+    size_maze = [4, 6]
     # initial position of the agent
     # all position count from 0
-    init_pos = [0, 7]
+    init_pos = [0, 0]
 
     # initiate maze simulator
     maze = MazeSimulator(size_maze[1], size_maze[0], init_pos, is_render)
@@ -79,11 +110,20 @@ if __name__ == "__main__":
 
     # build the rendered maze
     maze.build_maze()
-    QLearner = QLearningTable(actions=list(range(maze.n_actions)))
+
+    # initiate QLearner
+    actions = list(range(maze.n_actions))
+    learning_rate = 0.01
+    reward_gamma = 0.9
+    greedy = 0.99
+    QLearner = QLearningTable(actions, learning_rate, reward_gamma, greedy)
 
     # run the simulation of training
     if is_render:
-        maze.after(1, running(episodes, interval, is_render, QLearner, maze))
+        maze.after(1, learning(episodes, interval, is_render, QLearner, maze))
         maze.mainloop()
     else:
-        running(episodes, interval, is_render, QLearner, maze)
+        learning(episodes, interval, is_render, QLearner, maze)
+
+    # running(50, interval, is_render, QLearner, maze)
+
